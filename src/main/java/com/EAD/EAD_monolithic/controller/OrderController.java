879a8d27871db5cfd22dd676bbf53fc733a1dfb7
Navindu <dahanayakenw.20@uom.lk>
2023-11-07 18:30:08 +0530

    added orderSave controller and service

--- src/main/java/com/EAD/EAD_monolithic/controller/OrderController.java
+++ src/main/java/com/EAD/EAD_monolithic/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.EAD.EAD_monolithic.controller;
 
+import com.EAD.EAD_monolithic.dto.OrderDTO;
+import com.EAD.EAD_monolithic.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,14 +10,18 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/getOrder")
     public String getOrder(){
         return "getOrder";
     }
 
     @PostMapping("/saveOrder")
-    public String saveOrder(){
-        return "saveOrder";
+    public OrderDTO saveOrder(@RequestBody OrderDTO orderDTO){
+        orderService.saveOrder(orderDTO);
+        return null;
     }
 
     @PutMapping("/updateOrder")
--- src/main/java/com/EAD/EAD_monolithic/service/OrderService.java
+++ src/main/java/com/EAD/EAD_monolithic/service/OrderService.java
@@ -1,4 +1,25 @@
 package com.EAD.EAD_monolithic.service;
 
+
+import com.EAD.EAD_monolithic.dto.OrderDTO;
+import com.EAD.EAD_monolithic.entity.Order;
+import com.EAD.EAD_monolithic.repo.OrderRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class OrderService {
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public OrderDTO saveOrder(OrderDTO orderDTO){
+        orderRepo.save(modelMapper.map(orderDTO, Order.class));
+        return orderDTO;
+    }
 }
