3ebe9705863140b31219b88fad0d9bf1f20c8db1
Navindu <dahanayakenw.20@uom.lk>
2023-11-07 15:43:09 +0530

    Update OrderController.java
    Dummy order controllers

--- src/main/java/com/EAD/EAD_monolithic/controller/OrderController.java
+++ src/main/java/com/EAD/EAD_monolithic/controller/OrderController.java
@@ -1,4 +1,31 @@
 package com.EAD.EAD_monolithic.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/order")
+@CrossOrigin
 public class OrderController {
+
+    @GetMapping("/getOrder")
+    public String getOrder(){
+        return "getOrder";
+    }
+
+    @PostMapping("/saveOrder")
+    public String saveOrder(){
+        return "saveOrder";
+    }
+
+    @PutMapping("/updateOrder")
+    public String updateOrder(){
+        return "updateOrder";
+    }
+
+    @DeleteMapping("/deleteOrder")
+    public String deleteOrder(){
+        return "deleteOrder";
+    }
+
+
 }
