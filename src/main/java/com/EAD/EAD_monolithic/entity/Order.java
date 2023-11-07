d148eff5f709392cd85922c46662f177e74c3f07
Navindu <dahanayakenw.20@uom.lk>
2023-11-07 15:42:45 +0530

    Update Order.java
    Order entity created

--- src/main/java/com/EAD/EAD_monolithic/entity/Order.java
+++ src/main/java/com/EAD/EAD_monolithic/entity/Order.java
@@ -1,4 +1,29 @@
 package com.EAD.EAD_monolithic.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Order {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_sequence"
+    )
+    @SequenceGenerator(
+            name = "order_sequence",
+            sequenceName = "order_sequence",
+            allocationSize = 1
+    )
+
+    private String orderId;
+    private String userId;
+    private Double totalPrice;
+    private Boolean isPrepared;
 }
