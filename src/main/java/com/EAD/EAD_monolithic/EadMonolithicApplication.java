81b136217cac8848951ac9088842eae61828934c
Navindu <dahanayakenw.20@uom.lk>
2023-11-07 10:58:07 +0530

    initial commit

---
+++ src/main/java/com/EAD/EAD_monolithic/EadMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.EAD.EAD_monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadMonolithicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EAD/EAD_monolithic/EadMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EAD.EAD_monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
