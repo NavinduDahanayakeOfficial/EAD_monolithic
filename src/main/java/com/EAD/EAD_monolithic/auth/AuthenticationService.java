a60b23a2c63264fc9bcfe7ea2a1fc7f0a9cf8c43
Navindu <dahanayakenw.20@uom.lk>
2023-11-11 02:12:28 +0530

    Update AuthenticationService.java

--- src/main/java/com/EAD/EAD_monolithic/auth/AuthenticationService.java
+++ src/main/java/com/EAD/EAD_monolithic/auth/AuthenticationService.java
@@ -4,7 +4,7 @@ import com.EAD.EAD_monolithic.Exception.EmailAlreadyExistException;
 import com.EAD.EAD_monolithic.Exception.EmailOrPasswordIncorrectException;
 import com.EAD.EAD_monolithic.config.JwtService;
 import com.EAD.EAD_monolithic.entity.User;
-import com.EAD.EAD_monolithic.repo.UserRepository;
+import com.EAD.EAD_monolithic.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,7 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final UserRepository repository;
+    private final UserRepo repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
