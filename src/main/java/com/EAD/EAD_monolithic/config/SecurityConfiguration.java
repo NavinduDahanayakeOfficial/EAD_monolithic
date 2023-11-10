d3fe762c6dea7468194bf5063feed131685cfc32
Navindu <dahanayakenw.20@uom.lk>
2023-11-10 23:37:37 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/EAD/EAD_monolithic/config/SecurityConfiguration.java
+++ src/main/java/com/EAD/EAD_monolithic/config/SecurityConfiguration.java
@@ -32,8 +32,8 @@ public class SecurityConfiguration {
                     .permitAll()
 
 
-                .requestMatchers("/api/v1/order/**").hasAnyRole(ADMIN.name(), CUSTOMER.name(), DELIVERY_PERSON.name(), INVENTORY_KEEPER.name())
-                .requestMatchers(GET,"/api/v1/order/**").hasAnyAuthority(ADMIN_READ.name(), CUSTOMER_READ.name(), DELIVERY_PERSON_READ.name(), INVENTORY_KEEPER_READ.name())
+
+                .requestMatchers(GET,"/api/v1/order/**").hasAnyRole(ADMIN.name(), CUSTOMER.name(), DELIVERY_PERSON.name())
                 .requestMatchers(POST,"/api/v1/order/**").hasAnyAuthority(ADMIN_CREATE.name(), CUSTOMER_CREATE.name())
                 .requestMatchers(PUT,"/api/v1/order/**").hasAnyAuthority(ADMIN.name())
                 .requestMatchers(DELETE,"/api/v1/order/**").hasAnyAuthority(ADMIN_DELETE.name())
