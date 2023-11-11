28abd8a7666a87db3482de532741957e0494e721
Erandhi Madhushika <madhushikabae.20@uom.lk>
2023-11-11 13:01:17 +0530

    Add Security Configuration for Delivery

--- src/main/java/com/EAD/EAD_monolithic/config/SecurityConfiguration.java
+++ src/main/java/com/EAD/EAD_monolithic/config/SecurityConfiguration.java
@@ -43,6 +43,11 @@ public class SecurityConfiguration {
                 .requestMatchers(PUT,"/api/v1/product/**").hasAnyAuthority(ADMIN_UPDATE.getPermission())
                 .requestMatchers(DELETE,"/api/v1/product/**").hasAnyAuthority(ADMIN_DELETE.getPermission())
 
+                .requestMatchers(GET,"/api/v1/delivery/**").hasAnyAuthority(ADMIN_READ.getPermission(), CUSTOMER_READ.getPermission(), DELIVERY_PERSON_READ.getPermission())
+                .requestMatchers(POST,"/api/v1/delivery/**").hasAnyAuthority(ADMIN_CREATE.getPermission(), INVENTORY_KEEPER_CREATE.getPermission())
+                .requestMatchers(PATCH,"/api/v1/delivery/**").hasAnyAuthority(ADMIN_UPDATE.getPermission())
+                .requestMatchers(DELETE,"/api/v1/delivery/**").hasAnyAuthority(ADMIN_DELETE.getPermission(), INVENTORY_KEEPER_CREATE.getPermission())
+
                 .anyRequest()
                 .authenticated()
                 .and()
