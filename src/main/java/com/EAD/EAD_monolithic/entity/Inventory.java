7ea43d9f4444943391ef843e7b581ac1efcab227
KARkavinda <kavindakar.20@uom.lk>
2024-01-07 07:01:01 +0530

    addingPackages

--- src/main/java/com/EAD/EAD_monolithic/entity/Inventory.java
+++ src/main/java/com/EAD/EAD_monolithic/entity/Inventory.java
@@ -1,10 +1,14 @@
+package com.EAD.EAD_monolithic.entity;
+
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
